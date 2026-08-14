a2ab5e81bc6cd14ca71cb2c38ca0c434a3356a3f
santonese <133618906+santonese@users.noreply.github.com>
2026-08-14 10:03:28 +0800

    路由切换

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -3,3 +3,12 @@
     <router-view />
   </div>
 </template>
+<script>
+export default {
+  created() {
+    window.$wujie?.bus.$on("routeChange", (path) => {
+      this.$router.push(path);
+    });
+  },
+};
+</script>
